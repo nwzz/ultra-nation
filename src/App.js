d9532fea7bd499b99715ff28ed5af1eb459a0acb
Rabeya Begum <shahnewaz@mail.com>
2021-03-01 01:53:06 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [countries,setCountries] = useState([]);
+  const [cart,setCart] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data =>{
+     setCountries(data);
+     const names = data.map(country => country.name)
+    })
+    .catch(error => console.log(error))
+  },[])
+
+  const handleAddCountry = (country) =>{
+    const newCart = [...cart, country];
+    setCart(newCart);
+  }
   return (
     <div className="App">
+      <h2>Country Loaded : {countries.length}</h2>
+      <h3>Country added: {cart.length}</h3>
+      <Cart cart={cart}></Cart>
+      <div className="app-body">
+        {countries.map(country => <Country country = {country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)}
+      </div>
       
     </div>
   );
---
+++ src/components/Country/Country.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Country = (props) => {
+    console.log(props.country);
+    const {name, region, population,flag} = props.country;
+    const flagStyle = {height:'50px'};
+    const countryStyle= {border:'1px solid red', margin:'10px', padding:'10px'};
+    const handleAddCountry = props.handleAddCountry;
+    return (
+        <div style={countryStyle}>
+            <h4>Country: {name}</h4>
+            <img style={flagStyle} src={flag} alt=""/>
+            <p>Population: {population}</p>
+            <p><small> Region: {region}</small></p>
+            <button onClick={() => handleAddCountry(props.country)}>Add Country</button>
+
+
+        </div>
+    );
+};
+
+export default Country;
